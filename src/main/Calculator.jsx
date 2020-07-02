5e487f01595cf8acc82fea0428c9f07528d9928c
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-07-01 23:14:17 -0300

    aplicação da lógica para função setOperation

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -20,11 +20,35 @@ export default class Calculator extends Component {
     this.setOperation = this.setOperation.bind(this);
     this.addDigit = this.addDigit.bind(this);
   }
+
   clearMemory() {
     this.setState({ ...initialState });
   }
 
-  setOperation(operation) {}
+  setOperation(operation) {
+    if (this.state.current === "0") {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
+  }
 
   addDigit(n) {
     if (n === "." && this.state.displayValue.includes(".")) {
