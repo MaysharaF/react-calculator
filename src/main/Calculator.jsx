f714c2091c202ecec75afb5c402a87a8914915c2
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-07-01 22:56:08 -0300

    implementando lógica da função addDigit

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -26,7 +26,25 @@ export default class Calculator extends Component {
 
   setOperation(operation) {}
 
-  addDigit(n) {}
+  addDigit(n) {
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+    }
+  }
+
   render() {
     return (
       <div className="calculator">
